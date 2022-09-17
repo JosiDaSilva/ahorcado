7690fccce6f92f761cd6d67a32eef22da37cd650
Josiele Da Silva <90045757+JosiDaSilva@users.noreply.github.com>
2022-09-17 09:14:21 -0300

    Update juego.js

--- juego.js
+++ juego.js
@@ -157,21 +157,3 @@ var palabras = [ "PROGRAMAR", "DESARROLLO", "VENTISCA", "COLECCION", "INFORME", 
   // Modal agregar palabra
 
   nuevoJuego()
-if(document.getElementById("addWord")){
-  var modal = document.getElementById("modal");
-  var button = document.getElementById("addWord");
-  var span = document.getElementByClassName("close");
-  var section = document.getElementsByTagName("section");
-  button.onclick  function() {
-  modal.estilos.display= "block";
-  section.estilos.position = "static";
-  section.estilos.height = "80%";
-  section.estilos.overflow = "hidden";
-  }
-  span.onclick = function(){
-  modal.estilos.display= "none";
-  container.estilos.position = "inherit";
-  container.estilos.height = "auto";
-  container.estilos.overflow = "visible";
-  }
-  }
